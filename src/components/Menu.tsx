0f695da80cb4d52f56826cf2a43cd4e3a2146717
infuriating <decimatedxd@gmail.com>
2023-09-12 16:14:46 +0200

    added a menu

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,5 +1,26 @@
+import Link from "next/link";
 import React from "react";
 
 export default function Menu() {
-  return <div>Menu</div>;
+  return (
+    <div className="fixed bottom-4 z-20 ml-[5%] h-16 w-[90%] rounded-xl bg-white text-black">
+      <div className="grid h-full grid-cols-3  text-2xl font-semibold">
+        <Link href={"/"}>
+          <div className="flex h-full items-center justify-center">
+            <p>Home</p>
+          </div>
+        </Link>
+        <Link href={"/"}>
+          <div className="flex h-full items-center justify-center">
+            <p>Home</p>
+          </div>
+        </Link>
+        <Link href={"/"}>
+          <div className="flex h-full items-center justify-center">
+            <p>Home</p>
+          </div>
+        </Link>
+      </div>
+    </div>
+  );
 }
