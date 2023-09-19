558f824b04537b86e9618ebed2657de3561213da
infuriating <decimatedxd@gmail.com>
2023-09-19 16:37:22 +0200

    added background image to the outofbounds comp

--- src/components/OutOfBounds.tsx
+++ src/components/OutOfBounds.tsx
@@ -1,15 +1,25 @@
 "use client";
 
+import Image from "next/image";
 import React from "react";
 
 export default function OutOfBounds() {
   return (
     <div className="fixed flex h-screen w-screen items-center justify-center">
-      <button onClick={() => location.reload()} className="text-center">
-        <p className="text-6xl font-bold text-red-500">Out of bounds</p>
-        <p className="text-6xl font-bold text-red-500 underline">
-          Please refresh the page.
-        </p>
+      <Image
+        className="fixed -z-20 h-screen w-screen object-cover blur-md"
+        src={"/images/destiny2_director.jpeg"}
+        alt="Destiny 2 Director"
+        width={1920}
+        height={1080}
+        layout="intrinsic"
+      />
+      <button
+        onClick={() => location.reload()}
+        className="rounded-md bg-red-400 px-6 py-2 text-center"
+      >
+        <p className="text-6xl font-bold">Out of bounds</p>
+        <p className="text-6xl font-bold">Please refresh the page.</p>
       </button>
     </div>
   );
