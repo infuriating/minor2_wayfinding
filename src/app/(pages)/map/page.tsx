3e7bb345c409779314abf560c567058050e26059
infuriating <decimatedxd@gmail.com>
2023-09-19 15:23:02 +0200

    added OutOfBounds component and  viewport w+h

--- src/app/(pages)/map/page.tsx
+++ src/app/(pages)/map/page.tsx
@@ -1,12 +1,16 @@
 import MapImage from "@/components/MapImage";
 import React from "react";
 import Menu from "@/components/Menu";
+import OutOfBounds from "@/components/OutOfBounds";
 
 export default function page() {
   return (
     <>
-      <MapImage />
-      {/* <Menu /> */}
+      <OutOfBounds />
+      <div className="h-screen w-screen overflow-hidden">
+        <MapImage />
+        {/* <Menu /> */}
+      </div>
     </>
   );
 }
