7e44530facc479ef7a021224630a23d72825bed6
infuriating <decimatedxd@gmail.com>
2023-09-26 13:55:21 +0200

    small adjustments to the menu

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -3,21 +3,21 @@ import React from "react";
 
 export default function Menu() {
   return (
-    <div className="fixed bottom-4 z-20 ml-[5%] h-16 w-[90%] rounded-xl bg-white text-black">
-      <div className="grid h-full grid-cols-3  text-2xl font-semibold">
+    <div className="fixed bottom-4 z-10 ml-[5%] h-16 w-[90%] rounded-xl bg-white text-black">
+      <div className="grid h-full grid-cols-3 text-2xl font-semibold">
         <Link href={"/"}>
           <div className="flex h-full items-center justify-center">
             <p>Home</p>
           </div>
         </Link>
-        <Link href={"/"}>
+        <Link href={"/locations"}>
           <div className="flex h-full items-center justify-center">
-            <p>Home</p>
+            <p>Locations</p>
           </div>
         </Link>
-        <Link href={"/"}>
+        <Link href={"https://github.com/infuriating/minor2_wayfinding"}>
           <div className="flex h-full items-center justify-center">
-            <p>Home</p>
+            <p>Repository</p>
           </div>
         </Link>
       </div>
