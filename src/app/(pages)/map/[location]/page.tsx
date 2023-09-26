163a85b1e705a406a6bf109f08b80ce9be899c22
infuriating <decimatedxd@gmail.com>
2023-09-26 13:54:29 +0200

    made the location page functional

--- src/app/(pages)/map/[location]/page.tsx
+++ src/app/(pages)/map/[location]/page.tsx
@@ -1,37 +1,57 @@
-"use client";
-
+import Buttons from "@/components/Buttons";
+import PageTransition from "@/components/PageTransition";
 import { Locations } from "@/lib/Locations";
 import Image from "next/image";
 import Link from "next/link";
 
-import { usePathname } from "next/navigation";
-import { useEffect, useState } from "react";
+export async function generateMetadata({
+  params,
+}: {
+  params: { location: string };
+}) {
+  return {
+    title: `Destiny 2 Wayfinding | ${params.location}`,
+  };
+}
 
-const Location: React.FC = () => {
-  const pathname = usePathname();
-  const [filteredLocation, setFilteredLocation] = useState(null);
-  const locationName = pathname.split("/")[3];
+export default function Page({ params }: { params: { location: string } }) {
+  const location = Locations.find((l) => l.name === params.location);
 
-  useEffect(() => {
-    const filteredLocation = Locations.filter(
-      (location) => location.name === locationName,
+  if (!location) {
+    return (
+      <Link href={"/map"} className="flex h-screen items-center justify-center">
+        <div className="w-max rounded-md bg-neutral-900 px-4 py-1 font-medium transition-all hover:bg-neutral-800">
+          Location not found. Return to the map.
+        </div>
+      </Link>
     );
-    // setFilteredLocation(filteredLocation);
-    console.log(filteredLocation);
-  }, [locationName]);
-
+  }
   return (
-    <div className="flex h-screen w-screen items-center justify-center">
-      {filteredLocation || (
-        <Link href={"/"}>
-          <p className="w-max rounded-md bg-neutral-900 px-6 py-2 text-2xl font-medium transition-all hover:bg-neutral-800">
-            This page is currently not functional, please return back to the
-            homepage.
+    <>
+      <PageTransition />
+      <div className="p-8">
+        <p className="text-2xl font-semibold">{location?.name}</p>
+        <p className="tracking-tighter text-neutral-300">
+          {location?.description}
+        </p>
+        <Image
+          className="rounded-md"
+          src={
+            "/locations/destiny2_" +
+            // @ts-ignore
+            location?.name.replace(" ", "") +
+            ".png"
+          }
+          alt={location?.name}
+          height={500}
+          width={500}
+        />
+        <Link href={`/map`} className="flex items-center justify-center">
+          <p className="absolute bottom-[2rem] w-max cursor-pointer rounded-md bg-neutral-800 px-32 py-4 hover:bg-neutral-700">
+            Return to the map
           </p>
         </Link>
-      )}
-    </div>
+      </div>
+    </>
   );
-};
-
-export default Location;
+}
