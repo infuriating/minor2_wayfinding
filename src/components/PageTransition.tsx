52cd682e219eff43d6c011eb29168ed497ad1e11
infuriating <decimatedxd@gmail.com>
2023-09-26 14:01:49 +0200

    added gradient, changed timing

--- src/components/PageTransition.tsx
+++ src/components/PageTransition.tsx
@@ -8,7 +8,7 @@ export default function PageTransition() {
   useEffect(() => {
     setTimeout(() => {
       setTransition(false);
-    }, 1500);
+    }, 1750);
   }, []);
 
   return (
@@ -16,28 +16,28 @@ export default function PageTransition() {
       {transition && (
         <div className="fixed z-50 grid h-screen w-screen grid-rows-6">
           <div
-            className="animate-transitionOut h-full w-full bg-neutral-800"
+            className="animate-transitionOut h-full w-full bg-gradient-to-tr from-black to-neutral-900"
             style={{ animationDelay: "0ms" }}
           ></div>
           <div
-            className="animate-transitionOut h-full w-full bg-neutral-900"
-            style={{ animationDelay: "50ms" }}
-          ></div>
-          <div
-            className="animate-transitionOut h-full w-full bg-neutral-800"
+            className="animate-transitionOut h-full w-full bg-gradient-to-tr from-neutral-900 to-neutral-800"
             style={{ animationDelay: "100ms" }}
           ></div>
           <div
-            className="animate-transitionOut h-full w-full bg-neutral-900"
-            style={{ animationDelay: "150ms" }}
-          ></div>
-          <div
-            className="animate-transitionOut h-full w-full bg-neutral-800"
+            className="animate-transitionOut h-full w-full bg-gradient-to-tr from-black to-neutral-900"
             style={{ animationDelay: "200ms" }}
           ></div>
           <div
-            className="animate-transitionOut h-full w-full bg-neutral-900"
-            style={{ animationDelay: "250ms" }}
+            className="animate-transitionOut h-full w-full bg-gradient-to-tr from-neutral-900 to-neutral-800"
+            style={{ animationDelay: "300ms" }}
+          ></div>
+          <div
+            className="animate-transitionOut h-full w-full bg-gradient-to-tr from-black to-neutral-900"
+            style={{ animationDelay: "400ms" }}
+          ></div>
+          <div
+            className="animate-transitionOut h-full w-full bg-gradient-to-tr from-neutral-900 to-neutral-800"
+            style={{ animationDelay: "500ms" }}
           ></div>
         </div>
       )}
