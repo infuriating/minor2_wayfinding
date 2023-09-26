98f524fb6e73923bbd57e2d6046e8a646314da22
infuriating <decimatedxd@gmail.com>
2023-09-26 13:53:56 +0200

    added a button to locations page

--- src/app/(pages)/page.tsx
+++ src/app/(pages)/page.tsx
@@ -6,15 +6,20 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <div className="animate-darkenIn absolute z-10 h-screen w-screen bg-neutral-900 backdrop-blur-lg">
+      <div className="absolute z-10 h-screen w-screen animate-darkenIn bg-neutral-900 backdrop-blur-lg">
         <div className="flex h-full items-center justify-center">
           <p>envision the darkness within you.</p>
         </div>
       </div>
-      <div className="flex h-screen flex-col items-center justify-center">
+      <div className="flex h-screen flex-col items-center justify-center gap-2">
         <Link href={"/map"}>
           <p className="w-max rounded-md bg-neutral-900 px-4 py-1 font-medium transition-all hover:bg-neutral-800">
-            Enter the director
+            Enter the <span className="font-bold">Director</span>
+          </p>
+        </Link>
+        <Link href={"/locations"}>
+          <p className="w-max rounded-md bg-neutral-900 px-4 py-1 font-medium transition-all hover:bg-neutral-800">
+            View all the <span className="font-bold">Destiny 2</span> locations
           </p>
         </Link>
       </div>
