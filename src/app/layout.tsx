b9b8612f34f76aeeb14f74374f72ed195f65be4d
infuriating <infuriatingx1@gmail.com>
2023-10-02 13:41:38 +0200

    added icon to the layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/images/destiny2_icon.png" />
+      </head>
       <body className="font-poppins">{children}</body>
     </html>
   );
